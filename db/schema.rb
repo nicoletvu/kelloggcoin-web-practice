01dba374a6e94ddd2043d5c9c4786aeab9275ad6
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-05-12 04:45:39 +0000

    Finished Step 1 but it won't display

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_201519) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_04_201519) do
   create_table "transactions", force: :cascade do |t|
     t.integer "from_user_id"
     t.integer "to_user_id"
     t.integer "amount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
